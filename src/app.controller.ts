bc5f030d4e46de6e7f306995741ad7fac1f82690
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2023-02-14 11:49:53 -0300

    add/ new services

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Logger } from '@nestjs/common';
-import { EventPattern, Payload } from '@nestjs/microservices';
+import { Controller, Get, Logger, Query } from '@nestjs/common';
+import { EventPattern, MessagePattern, Payload } from '@nestjs/microservices';
 import { AppService } from './app.service';
 import { Categoria } from './interfaces/categorias/categoria.interface';
 
@@ -12,11 +12,17 @@ export class AppController {
   @EventPattern('criar-categoria')
   async criarCategoria(
     @Payload() categoria: Categoria) {
-
-      this.logger.log(`categoria: ${JSON.stringify(categoria)}`);
-      this.appService.criarCategoria(categoria);
-
+      this.logger.log(`categoria: ${categoria}`);
+      await this.appService.criarCategoria(categoria);
   }
 
+  @MessagePattern('consultar-categorias')
+  async consultarCategorias(@Payload() _id: string) {
+    if(_id) {
+      return await this.appService.consultarCategoriaPeloId(_id);
+    } else {
+      return await this.appService.consultarTodasCategorias();
+    }
+  }
 
 }
