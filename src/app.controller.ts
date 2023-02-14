0d70ddc03c94d613120adbca59b540db90d2bcf1
Vinicius Pimentel <vinicius.lopes.pimentel@gmail.com>
2023-02-14 15:35:14 -0300

    add/ refactoring

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,6 @@
 import { Controller, Get, Logger, Query } from '@nestjs/common';
 import { Ctx, EventPattern, MessagePattern, Payload, RmqContext } from '@nestjs/microservices';
+import { channel } from 'diagnostics_channel';
 import { AppService } from './app.service';
 import { Categoria } from './interfaces/categorias/categoria.interface';
 
@@ -7,7 +8,7 @@ const ackErrors: string[] = ['E11000'];
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   logger = new Logger(AppController.name);
 
@@ -15,28 +16,29 @@ export class AppController {
   async criarCategoria(
     @Payload() categoria: Categoria, @Ctx() context: RmqContext) {
 
-      // const channel = context.getChannelRef();
-      // const originalMsg = context.getMessage();
+    // const channel = context.getChannelRef();
+    // const originalMsg = context.getMessage();
 
-      this.logger.log(`categoria: ${categoria}`);
+    this.logger.log(`categoria: ${categoria}`);
 
-      try {
-        await this.appService.criarCategoria(categoria);
-        // await channel.ack(originalMsg); // delete stopped messages in the queue
-      } catch (err) {
-        this.logger.error(`error: ${err.message}`);
-        // ackErrors.map(async ackError => {
-        //   if(err.message.includes(ackError)){
-        //     await channel.ack(originalMsg);
-        //   }
-        // })
-      }
+    try {
+      await this.appService.criarCategoria(categoria);
+      // await channel.ack(originalMsg); // delete stopped messages in the queue
+    } catch (err) {
+      this.logger.error(`error: ${err.message}`);
+      // const filterAckError = ackErrors.filter(
+      //   ackError => err.message.includes(ackError))
+
+      // if(filterAckError) {
+      //   await channel.ack(originalMsg);
+      // }
+    }
 
   }
 
   @MessagePattern('consultar-categorias')
   async consultarCategorias(@Payload() _id: string) {
-    if(_id) {
+    if (_id) {
       return await this.appService.consultarCategoriaPeloId(_id);
     } else {
       return await this.appService.consultarTodasCategorias();
